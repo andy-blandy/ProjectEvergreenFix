b7d8c21a585ef26c13a6e415ecfe794c4658c2b8
GlassDragon37 <adele.dorothy24@gmail.com>
2023-10-26 16:10:22 -0400

    Added scripts

--- Assets/Scripts/Placeable Objects/Fountain.cs
+++ Assets/Scripts/Placeable Objects/Fountain.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Fountain : Building
 {
+    int environmentalImpact = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameManager.instance.subtractImpact(environmentalImpact);
     }
 
     // Update is called once per frame
--- Assets/Scripts/Placeable Objects/Library.cs
+++ Assets/Scripts/Placeable Objects/Library.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Library : Building
 {
+    bool isPowered = false;
+    int happiness = 10;
+    int jobs = 20;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameManager.instance.addHappiness(happiness);
+        GameManager.instance.addJobs(jobs);
     }
 
     // Update is called once per frame
